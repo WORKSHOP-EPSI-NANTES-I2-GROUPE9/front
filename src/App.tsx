5cd2b2585bb6e5c7bd031a2158488d8cab034573
Ked57 <kedketh@gmail.com>
2020-11-25 13:49:14 +0100

    Add CI

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,38 @@ const App: React.FC = () => {
   const changeTranslation = (locale: string) => i18n.changeLanguage(locale);
 
   return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <div>
-              <Link to="/" className="App-header-item">
-                {t("nav.home")}
-              </Link>
-            </div>
-            <div>
-              <a
-                className="App-header-item-translation cy-translation-en"
-                onClick={() => changeTranslation("en")}
-              >
-                EN
-              </a>
-              <a
-                className="App-header-item-translation cy-translation-fr"
-                onClick={() => changeTranslation("fr")}
-              >
-                FR
-              </a>
-            </div>
-          </header>
-          <div className="content">
-            <Switch>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Link to="/" className="App-header-item">
+              {t("nav.home")}
+            </Link>
           </div>
+          <div>
+            <a
+              className="App-header-item-translation cy-translation-en"
+              onClick={() => changeTranslation("en")}
+            >
+              EN
+            </a>
+            <a
+              className="App-header-item-translation cy-translation-fr"
+              onClick={() => changeTranslation("fr")}
+            >
+              FR
+            </a>
+          </div>
+        </header>
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
 };
 
